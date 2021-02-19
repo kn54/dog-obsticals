faf1ccbb8be0c2d3b637b96f59a1844745603f68
kaia neuman <daniellneuman@gmail.com>
2021-02-19 22:14:59 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+function preload() {
+    this.load.image('player', 'assets/repl.png');
+}
+
+function create() {
+    this.w = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
+    this.a = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
+    this.s = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
+    this.d = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
+    this.j = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.J)
+
+    this.player = this.physics.add.image(config.width / 2, config.height / 2, 'player').setScale(0.25, 0.25);
+    this.player.setCollideWorldBounds(true);
+}
+
+function update() {
+    let cursors = this.input.keyboard.createCursorKeys();
+    if ( this.a.isDown || this.d.isDown)this.player.setVelocityX(this.a.isDown ? -160 : 160);
+    else this.player.setVelocityX(0);
+    if (this.w.isDown ||this.s.isDown) this.player.setVelocityY(this.w.isDown ? -160 : 160);
+    else this.player.setVelocityY(0);
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 500,
+    height: 400,
+    backgroundColor: '#f9f9f9',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                y: 0
+            },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(config);
